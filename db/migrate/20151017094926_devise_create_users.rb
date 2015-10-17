aa3e7bb32cd18125df2323e3287cd61c41ead006
rakesh <khicherrakesh10@gmail.com>
2015-10-17 17:49:08 +0530

    modified migration added

--- db/migrate/20151017094926_devise_create_users.rb
+++ db/migrate/20151017094926_devise_create_users.rb
@@ -4,7 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :name
+      t.string :mobile_number
+      t.index :mobile_number, unique: true
+      t.string :address
+      t.string :type
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
